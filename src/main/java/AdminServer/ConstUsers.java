189abae52dbf71d1191bb23e8cd988e677d098c0
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2021-12-06 17:43:54 +0300

    Add: Add UserConst, DatabaseHandlerUsers,Users

---
+++ src/main/java/AdminServer/ConstUsers.java
@@ -0,0 +1,14 @@
+package AdminServer;
+
+public class ConstUsers {
+    public static final String USER_TABLE = "accounts";
+
+    public static final String USER_ID = "id";
+    public static final String USER_NAME = "firstname";
+    public static final String USER_SURNAME = "lastname";
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PASSWORD = "password";
+    public static final String USER_EMAIL = "email";
+    public static final String USER_GENDER = "gender";
+    public static final String USER_LOCATION = "location";
+}
---
+++ src/main/java/AdminServer/Users.java
@@ -0,0 +1,100 @@
+package AdminServer;
+
+public class Users {
+    private String user_id;
+    private String user_firstname;
+    private String user_lastname;
+    private String user_login;
+    private String user_password;
+    private String user_email;
+    private String user_gender;
+    private String user_location;
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "user_id='" + user_id + '\'' +
+                ", user_firstname='" + user_firstname + '\'' +
+                ", user_lastname='" + user_lastname + '\'' +
+                ", user_login='" + user_login + '\'' +
+                ", user_password='" + user_password + '\'' +
+                ", user_email='" + user_email + '\'' +
+                ", user_gender='" + user_gender + '\'' +
+                ", user_location='" + user_location + '\'' +
+                '}';
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_firstname() {
+        return user_firstname;
+    }
+
+    public void setUser_firstname(String user_firstname) {
+        this.user_firstname = user_firstname;
+    }
+
+    public String getUser_lastname() {
+        return user_lastname;
+    }
+
+    public void setUser_lastname(String user_lastname) {
+        this.user_lastname = user_lastname;
+    }
+
+    public String getUser_login() {
+        return user_login;
+    }
+
+    public void setUser_login(String user_login) {
+        this.user_login = user_login;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_gender() {
+        return user_gender;
+    }
+
+    public void setUser_gender(String user_gender) {
+        this.user_gender = user_gender;
+    }
+
+    public String getUser_location() {
+        return user_location;
+    }
+
+    public void setUser_location(String user_location) {
+        this.user_location = user_location;
+    }
+
+    public Users(String user_firstname, String user_lastname, String user_login, String user_password, String user_email, String user_gender, String user_location) {
+        this.user_firstname = user_firstname;
+        this.user_lastname = user_lastname;
+        this.user_login = user_login;
+        this.user_password = user_password;
+        this.user_email = user_email;
+        this.user_gender = user_gender;
+        this.user_location = user_location;
+    }
+}
